d76ea55368ebe2566d6e7382766baecc5f755041
CJ Jessett <cjessett@gmail.com>
2016-07-31 15:27:15 -0500

    update routes, add delete and edit functionality

--- controllers/todos.rb
+++ controllers/todos.rb
@@ -1,9 +1,31 @@
 get '/' do
+  redirect '/todos'
+end
+
+get '/todos' do
   @todos = Todos.all
   erb :index
 end
 
-post '/' do
+post '/todos' do
   Todos.create(description: params[:description])
   redirect '/'
+end
+
+get '/todos/:id' do
+  @todo = Todos.find(params[:id])
+  erb :edit
+end
+
+put '/todos/:id' do
+  todo = Todos.find(params[:id])
+  todo.description = params[:description]
+  todo.save
+  redirect '/todos'
+end
+
+get '/todos/:id/delete' do
+  todo = Todos.find(params[:id])
+  todo.destroy
+  redirect '/todos'
 end
